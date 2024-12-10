ca6d5b69328c05f7054b1566dab56904425fd321
Jim Groome <james@jgroome.com>
2024-12-10 16:53:12 +0000

    Resolving dependency issues

--- payload.config.ts
+++ payload.config.ts
@@ -20,31 +20,17 @@ export default buildConfig({
   admin: {
     user: "admins",
   },
-  // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor({
     features: ({ defaultFeatures, rootFeatures }) => [
       ...defaultFeatures,
       EXPERIMENTAL_TableFeature(),
-      // TableFeatureClient({
-
-      // }),
-      // LinkFeature,
     ],
   }),
-  // Define and configure your collections in this array
   collections: [Publications, Admins, Users],
-
-  // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || "",
-  // Whichever Database Adapter you're using should go here
-  // Mongoose is shown as an example, but you can also use Postgres
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || "",
   }),
-  // If you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-  // This is optional - if you don't need to do these things,
-  // you don't need it!
   sharp,
   localization: {
     locales: [
@@ -56,8 +42,8 @@ export default buildConfig({
         label: "French",
         code: "fr",
       },
-    ], // required
-    defaultLocale: "en", // required
+    ],
+    defaultLocale: "en",
   },
   plugins: [
     searchPlugin({
@@ -70,7 +56,6 @@ export default buildConfig({
       collections: ["publications"],
       globals: [],
       resolvers: [
-        copyResolver(),
         deeplTranslate({
           apiKey: process.env.DEEPL_API_KEY!,
         }),
@@ -78,7 +63,6 @@ export default buildConfig({
     }),
   ],
   i18n: {
-    // supportedLanguages: { en },
     translations: {
       en: {
         "plugin-translator": {
